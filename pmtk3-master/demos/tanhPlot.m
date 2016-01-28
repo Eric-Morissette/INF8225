08986355a45b9672226056dce5cb5dbdead9d2f9
Eric Morissette <eric-2.morissette@polymtl.ca>
2016-01-28 15:07:13 -0500

    Add Netica, pmtk3 and TP1 dir

---
+++ pmtk3-master/demos/tanhPlot.m
@@ -0,0 +1,22 @@
+%% Visually compare tanh and sigmoid functions
+%
+%%
+
+% This file is from pmtk3.googlecode.com
+[styles, colors, symbols, str] =  plotColors();
+xs = -3:0.1:3;
+names = {'tanh', 'sigmoid', 'relu', 'prelu', 'softpplus'};
+curves = {};
+curves{end+1} = tanh(xs);
+curves{end+1} = 1./(1+exp(-xs));
+curves{end+1} = max(0, xs);
+curves{end+1} = max(0, xs) + 0.1*min(0,xs);
+curves{end+1} = log(1+exp(xs));
+figure;
+hold on
+for i=1:numel(curves)
+plot(xs, curves{i}, str{i}, 'linewidth', 3);
+end
+legend(names, 'location', 'northwest');
+set(gca,'ylim',[-1 2]);
+printPmtkFigure('tanhPlot')
