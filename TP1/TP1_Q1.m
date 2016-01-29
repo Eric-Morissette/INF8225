9385586e1b1b4761772b5373c9c91f762c81f1ae
Eric Morissette <eric-2.morissette@polymtl.ca>
2016-01-29 14:13:08 -0500

    TP1 Files, Q1 and 2 partially done

--- TP1/TP1_Q1.m
+++ TP1/TP1_Q1.m
@@ -1,3 +1,11 @@
+fprintf('TP1 - Question 1:\n\n');
+
+fprintf(' [B]   [F]\n');
+fprintf('   \\   /\n');
+fprintf('    [G]\n');
+fprintf('   /   \\\n');
+fprintf(' [D]   [FT]\n\n');
+
 B = 1; F = 2; G = 3; D = 4; FT = 5;
 
 names = cell(1,5);
@@ -21,57 +29,58 @@ CPDs{FT} = tabularCpdCreate(reshape([0.2 0.6 0.8 0.4], 2, 2));
 dgm = dgmCreate(dgm, CPDs, 'nodenames', names, 'infEngine', 'jtree');
 joint = dgmInferQuery(dgm, [B,F,G,D,FT]);
 
-
-fprintf('Explaining away\n');
-
+fprintf('(1): Explaining Away\n');
 clampled = sparsevec(B,1,5);
-FGivenB = tabularFactorCondition(joint, F, clampled);
-fprintf('p(F|B=0)=%f\n', FGivenB.T(1));
+F_Sachant_B = tabularFactorCondition(joint, F, clampled);
+fprintf('F Sachant !B: p(F|B=0)=%f\n', F_Sachant_B.T(1));
 
 clampled = sparsevec(B,2,5);
-FGivenB = tabularFactorCondition(joint, F, clampled);
-fprintf('p(F|B=1)=%f\n', FGivenB.T(1));
-fprintf('Il ny a aucun changement pour F car B et F sont independant\nEn fixant G et B, on Explain Away F qui voit sa probabilite changer \n');
+F_Sachant_B = tabularFactorCondition(joint, F, clampled);
+fprintf('F Sachant  B: p(F|B=1)=%f\n', F_Sachant_B.T(1));
+fprintf('Il n''y a aucuns changements pour F sachant B car B et F ne sont pas dependants\n\n');
 
 clampled = sparsevec([B G],2,5);
-FGivenBG = tabularFactorCondition(joint, F, clampled);
-fprintf('p(F|B=1,G=1)=%f\n', FGivenBG.T(1));
+F_Sachant_B_G = tabularFactorCondition(joint, F, clampled);
+fprintf('F Sachant (B et G): p(F|B=1,G=1)=%f\n', F_Sachant_B_G.T(1));
+fprintf('En fixant G _et_ B, on peut voir la probabilite de F augmenter par un ''Explaining Away''\n\n\n');
 
-fprintf('Serial blocking\n');
+
+fprintf('(2): Serial Blocking\n');
 clampled = sparsevec(B, 1, 5);
-DGivenB = tabularFactorCondition(joint, D, clampled);
-fprintf('p(D|B=0)=%f\n', DGivenB.T(1));
+D_Sachant_B = tabularFactorCondition(joint, D, clampled);
+fprintf('D Sachant !B: p(D|B=0)=%f\n', D_Sachant_B.T(1));
 
 clampled = sparsevec(B, 2, 5);
-DGivenB = tabularFactorCondition(joint, D, clampled);
-fprintf('p(D|B=1)=%f\n', DGivenB.T(1));
-fprintf('B influence D\n');
+D_Sachant_B = tabularFactorCondition(joint, D, clampled);
+fprintf('D Sachant  B: p(D|B=1)=%f\n', D_Sachant_B.T(1));
+fprintf('On voit que B influence D\n\n');
 
-clampled =sparsevec([B G], 1, 5);
-DGivenBG = tabularFactorCondition(joint, D, clampled);
-fprintf('p(D|B=0, G=0)=%f\n', DGivenBG.T(1));
+clampled = sparsevec([B G], 1, 5);
+D_Sachant_B_G = tabularFactorCondition(joint, D, clampled);
+fprintf('D Sachant !B !G: p(D|B=0, G=0)=%f\n', D_Sachant_B_G.T(1));
 
-clampled =sparsevec([B G], [2 1], 5);
-DGivenBG = tabularFactorCondition(joint, D, clampled);
-fprintf('p(D|B=1, G=0)=%f\n', DGivenBG.T(1));
-fprintf('G bloque linfluence de B sur G');
+clampled = sparsevec([B G], [2 1], 5);
+D_Sachant_B_G = tabularFactorCondition(joint, D, clampled);
+fprintf('D Sachant  B !G: p(D|B=1, G=0)=%f\n', D_Sachant_B_G.T(1));
+fprintf('On voit que G bloque l''influence de B sur G\n\n\n');
 
-fprintf('Divergent blocking\n');
+
+fprintf('(3): Divergent Blocking\n');
 clampled = sparsevec(D,1,5);
-FTGivenD = tabularFactorCondition(joint, FT, clampled);
-fprintf('p(FT|D=0)=%f\n',FTGivenD.T(1));
+FT_Sachant_D = tabularFactorCondition(joint, FT, clampled);
+fprintf('FT Sachant !D: p(FT|D=0)=%f\n',FT_Sachant_D.T(1));
 
 clampled = sparsevec(D,2,5);
-FTGivenD = tabularFactorCondition(joint, FT, clampled);
-fprintf('p(FT|D=1)=%f\n',FTGivenD.T(1));
-fprintf('D influence FT\n');
+FT_Sachant_D = tabularFactorCondition(joint, FT, clampled);
+fprintf('FT Sachant  D: p(FT|D=1)=%f\n',FT_Sachant_D.T(1));
+fprintf('On voit que D influence FT\n\n');
 
 clampled = sparsevec([D G],1,5);
-FTGivenDG = tabularFactorCondition(joint, FT, clampled);
-fprintf('p(FT|D=0,G=0)=%f\n',FTGivenDG.T(1));
+FT_Sachant_D_G = tabularFactorCondition(joint, FT, clampled);
+fprintf('FT Sachant  D  G: p(FT|D=0,G=0)=%f\n',FT_Sachant_D_G.T(1));
 
 clampled = sparsevec([D G],[2 1],5);
-FTGivenDG = tabularFactorCondition(joint, FT, clampled);
-fprintf('p(FT|D=1,G=0)=%f\n',FTGivenDG.T(1));
-fprintf('G bloque linfluence de D sur FT\n');
+FT_Sachant_D_G = tabularFactorCondition(joint, FT, clampled);
+fprintf('FT Sachant  D !G: p(FT|D=1,G=0)=%f\n',FT_Sachant_D_G.T(1));
+fprintf('On voit que G bloque l''influence de D sur FT\n');
 
--- TP1/mkDetecterCambriolageDgm.m
+++ TP1/mkDetecterCambriolageDgm.m
@@ -1,3 +1,11 @@
+fprintf('TP1 - Question 2:\n\n');
+
+fprintf(' [C]   [T]\n');
+fprintf('   \\   /|\n');
+fprintf('    [A] |\n');
+fprintf('   /   \\|\n');
+fprintf(' [M]   [J]\n\n');
+
 C = 1; T = 2; A = 3; M = 4; J = 5;
 
 names = cell(1,5);
@@ -21,50 +29,52 @@ CPDs{M} = tabularCpdCreate(reshape([0.95 0.1 0.05 0.9], 2, 2));
 CPDs{J} = tabularCpdCreate(reshape([0.999 0.999 0.5 0.95 0.001 0.001 0.5 0.05], 2, 2, 2));
 
 dgm = dgmCreate(dgm, CPDs, 'nodenames', names, 'infEngine', 'jtree');
-joint = dgmInferQuery(dgm, [C,T,A,M,J]);
+joint = dgmInferQuery(dgm, [C, T, A, M, J]);
 
-fprintf('c)\n');
-clampled = sparsevec([M J],[2 1],5);
-CGivenMJ = tabularFactorCondition(joint, C, clampled);
-fprintf('p(C=1|M=1,J=0)=%f\n',CGivenMJ.T(2));
 
-clampled = sparsevec([M J],[1 2],5);
-CGivenMJ = tabularFactorCondition(joint, C, clampled);
-fprintf('p(C=1|M=0,J=1)=%f\n',CGivenMJ.T(2));
 
-clampled = sparsevec([M J],[2 2],5);
-CGivenMJ = tabularFactorCondition(joint, C, clampled);
-fprintf('p(C=1|M=1,J=1)=%f\n',CGivenMJ.T(2));
+fprintf('(C) - Marginales Conditionnelles\n');
+clampled = sparsevec([M J], [2 1], 5);
+C_Sachant_M_J = tabularFactorCondition(joint, C, clampled);
+fprintf('C Sachant !M  J: p(C=1|M=1,J=0)=%f\n', C_Sachant_M_J.T(2));
 
-clampled = sparsevec([M J],[1 1],5);
-CGivenMJ = tabularFactorCondition(joint, C, clampled);
-fprintf('p(C=1|M=0,J=0)=%f\n',CGivenMJ.T(2));
+clampled = sparsevec([M J], [1 2], 5);
+C_Sachant_M_J = tabularFactorCondition(joint, C, clampled);
+fprintf('C Sachant !M  J: p(C=1|M=0,J=1)=%f\n', C_Sachant_M_J.T(2));
 
-clampled = sparsevec(M,2,5);
-CGivenM = tabularFactorCondition(joint, C, clampled);
-fprintf('p(C=1|M=1)=%f\n',CGivenM.T(2));
+clampled = sparsevec([M J], [2 2], 5);
+C_Sachant_M_J = tabularFactorCondition(joint, C, clampled);
+fprintf('C Sachant  M  J: p(C=1|M=1,J=1)=%f\n', C_Sachant_M_J.T(2));
 
-clampled = sparsevec(J,2,5);
-CGivenJ = tabularFactorCondition(joint, C, clampled);
-fprintf('p(C=1|J=1)=%f\n',CGivenJ.T(2));
+clampled = sparsevec([M J], [1 1], 5);
+C_Sachant_M_J = tabularFactorCondition(joint, C, clampled);
+fprintf('C Sachant !M !J: p(C=1|M=0,J=0)=%f\n', C_Sachant_M_J.T(2));
 
-fprintf('d)\n');
-clampled = zeros(5,1);
-pC = tabularFactorCondition(joint, C, clampled);
-fprintf('p(C=1)=%f\n',pC.T(2));
+clampled = sparsevec(M, 2, 5);
+C_Sachant_M = tabularFactorCondition(joint, C, clampled);
+fprintf('C Sachant  M   : p(C=1|M=1)    =%f\n', C_Sachant_M.T(2));
 
-clampled = zeros(5,1);
-pT = tabularFactorCondition(joint, T, clampled);
-fprintf('p(T=1)=%f\n',pT.T(2));
+clampled = sparsevec(J, 2, 5);
+C_Sachant_J = tabularFactorCondition(joint, C, clampled);
+fprintf('C Sachant     J: p(C=1|J=1)    =%f\n\n', C_Sachant_J.T(2));
 
-clampled = zeros(5,1);
-pA = tabularFactorCondition(joint, A, clampled);
-fprintf('p(A=1)=%f\n',pA.T(2));
+fprintf('(D) - Marginales Inconditionnelles\n');
+clampled = zeros(5, 1);
+p_C = tabularFactorCondition(joint, C, clampled);
+fprintf('C: p(C=1)=%f\n', p_C.T(2));
 
-clampled = zeros(5,1);
-pM = tabularFactorCondition(joint, M, clampled);
-fprintf('p(M=1)=%f\n',pM.T(2));
+clampled = zeros(5, 1);
+p_T = tabularFactorCondition(joint, T, clampled);
+fprintf('T: p(T=1)=%f\n', p_T.T(2));
 
-clampled = zeros(5,1);
-pJ = tabularFactorCondition(joint, J, clampled);
-fprintf('p(J=1)=%f\n',pJ.T(2));
+clampled = zeros(5, 1);
+p_A = tabularFactorCondition(joint, A, clampled);
+fprintf('A: p(A=1)=%f\n', p_A.T(2));
+
+clampled = zeros(5, 1);
+p_M = tabularFactorCondition(joint, M, clampled);
+fprintf('M: p(M=1)=%f\n', p_M.T(2));
+
+clampled = zeros(5, 1);
+p_J = tabularFactorCondition(joint, J, clampled);
+fprintf('J: p(J=1)=%f\n', p_J.T(2));
