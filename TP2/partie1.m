7513395e1dff50227fd7ad18571ac3714b1a5a1a
Eric-Morissette <The__Momo@hotmail.com>
2016-02-14 20:57:10 -0500

    TP2 - Part 2

--- TP2/partie1.m
+++ TP2/partie1.m
@@ -13,10 +13,10 @@ i = 1:m;
 j = newsgroups;
 Y = sparse(i, j, o, m, n);
 
-theta = rand(4, 101) - 0.5;
-X = documents;
-X = [X; ones(1, 16242)];
-rate_Learning = 0.0005;
+theta           = rand(4, 101) - 0.5;
+X               = documents;
+X               = [X; ones(1, 16242)];
+rate_Learning   = 0.0005;
 
 % Split the data into Learning, Validation and Test data
 [X_Learning, X_Validation, X_Test, Y_Learning, Y_Validation, Y_Test] = create_train_valid_test_splits(X, Y);
@@ -27,10 +27,10 @@ rate_Learning = 0.0005;
 batch = Y_Learning' * X_Learning';
 
 itr = 0;
-logLikelihood = -realmax;
-precision_Learning = [];
-precision_Validation = [];
-logLikelihood_Batch = [];
+logLikelihood           = -realmax;
+precision_Learning      = [];
+precision_Validation    = [];
+logLikelihood_Batch     = [];
 
 loglikelihoodDifference = 2;
 while abs(loglikelihoodDifference) > 1
@@ -43,9 +43,9 @@ while abs(loglikelihoodDifference) > 1
     precision_Learning      = [precision_Learning precision(X_Learning, Y_Learning, theta)];
     precision_Validation    = [precision_Validation precision(X_Validation, Y_Validation, theta)];
 
-    expected = (exp( eye(4) * theta * X_Learning) ./ (repmat( sum( exp( eye(4) * theta * X_Learning) ), 4, 1))) * X_Learning';
-    gradient = expected - batch;
-    theta = theta - rate_Learning * gradient;
+    expected    = (exp( eye(4) * theta * X_Learning) ./ (repmat( sum( exp( eye(4) * theta * X_Learning) ), 4, 1))) * X_Learning';
+    gradient    = expected - batch;
+    theta       = theta - rate_Learning * gradient;
 
     itr = itr + 1;
 end
@@ -57,16 +57,16 @@ fprintf('Precision of the batch descent with test data = %f\n', precision(X_Test
 %%%%%%%%%%%%%%%%%%%%%
 %     Mini-Batch    %
 %%%%%%%%%%%%%%%%%%%%%
-theta = rand(4, 101) - 0.5;
-miniBatchSize = 568;
-alpha = 0.6;
-thetaDifference = zeros(4, 101);
-itr = 0;
-logLikelihood = -realmax;
-logLikelihood_MiniBatch = [];
-precision_Validation_MiniBatch = [];
-precision_Learning_MiniBatch = [];
-delta = 1;
+theta                           = rand(4, 101) - 0.5;
+miniBatchSize                   = 568;
+alpha                           = 0.6;
+thetaDifference                 = zeros(4, 101);
+itr                             = 0;
+logLikelihood                   = -realmax;
+logLikelihood_MiniBatch         = [];
+precision_Validation_MiniBatch  = [];
+precision_Learning_MiniBatch    = [];
+delta                           = 1;
 
 converged = false;
 while ~converged
@@ -75,16 +75,16 @@ while ~converged
 
     rate_Learning = 2 / itr;
     for i = 1:size( X_Batch, 2)
-        tempLogLikelihood = sum( ( sum( ( ( Y_Validation * theta ) .* X_Validation' )' ) ) - log( sum( exp( eye(4) * theta * X_Validation ) ) ) );
-        delta = tempLogLikelihood - logLikelihood;
-        logLikelihood = tempLogLikelihood;
+        tempLogLikelihood       = sum( ( sum( ( ( Y_Validation * theta ) .* X_Validation' )' ) ) - log( sum( exp( eye(4) * theta * X_Validation ) ) ) );
+        delta                   = tempLogLikelihood - logLikelihood;
+        logLikelihood           = tempLogLikelihood;
         logLikelihood_MiniBatch = [logLikelihood_MiniBatch logLikelihood];
 
-        expected = ( ( exp( eye(4) * theta * X_Batch{:,i} ) ) ./ ( repmat( sum( exp( eye(4) * theta * X_Batch{:,i} ) ), 4, 1 ) ) ) * X_Batch{:,i}';
-        batch = Y_Batch{i, :}' * X_Batch{:, i}';
-        gradient = (expected - batch) ./ miniBatchSize;
+        expected        = ( ( exp( eye(4) * theta * X_Batch{:,i} ) ) ./ ( repmat( sum( exp( eye(4) * theta * X_Batch{:,i} ) ), 4, 1 ) ) ) * X_Batch{:,i}';
+        batch           = Y_Batch{i, :}' * X_Batch{:, i}';
+        gradient        = (expected - batch) ./ miniBatchSize;
         thetaDifference = (alpha * thetaDifference) - (rate_Learning * gradient);
-        theta = theta + thetaDifference;
+        theta           = theta + thetaDifference;
 
         precision_Validation_MiniBatch = [precision_Validation_MiniBatch precision(X_Validation, Y_Validation, theta)];
 
@@ -114,12 +114,3 @@ title('Learning Curves');
 xlabel('Iteration');
 ylabel('Precision');
 legend('Learning Data (Batch)','Validation Data (Batch)', 'Validation Data (Mini-Batch)', 'Learning Data (Mini-Batch)');
-
-
-
-
-
-
-
-
-
---
+++ TP2/partie2.m
@@ -0,0 +1,186 @@
+% Clear console and workspace
+clc;
+clear;
+
+% Load the file
+load 20news_w100;
+
+% Initialize dimensions
+n = 4;
+m = size(newsgroups, 2);
+o = ones(1, m);
+i = 1:m;
+j = newsgroups;
+Y = sparse(i, j, o, m, n);
+
+X = documents;
+X = [ X; ones( 1, m ); randi( [0 1], 100, m ) ];
+
+% Split the data into Learning, Validation and Test data
+[ X_Learning, X_Validation, X_Test, Y_Learning, Y_Validation, Y_Test ] = create_train_valid_test_splits( X, Y );
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Mini-Batch without Regularization  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+theta           = rand(4, 201) - 0.5;
+miniBatchSize   = 568;
+alpha           = 0.6;
+thetaDifference = zeros(4, 201);
+itr             = 0;
+logLikelihood   = -realmax;
+
+converged = false;
+while ~converged
+    itr = itr + 1;
+    [ X_Batch, Y_Batch ] = create_mini_batches( X_Learning, Y_Learning, miniBatchSize );
+
+    learning_Rate = 2 / itr;
+    for i = 1:size( X_Batch, 2)
+        tempLogLikelihood   = sum( ( sum( ( ( Y_Validation * theta ) .* X_Validation' )' ) ) - log( sum( exp( eye(4) * theta * X_Validation ) ) ) );
+        delta               = tempLogLikelihood - logLikelihood;
+        logLikelihood       = tempLogLikelihood;
+
+        expected        = ( ( exp( eye(4) * theta * X_Batch{:,i} ) ) ./ ( repmat( sum( exp( eye(4) * theta * X_Batch{ :, i } ) ), 4, 1 ) ) ) * X_Batch{ :, i }';
+        batch           = Y_Batch{i, :}' * X_Batch{:, i}';
+        gradient        = ( expected - batch ) ./ miniBatchSize;
+        thetaDifference = ( alpha * thetaDifference ) - ( learning_Rate * gradient );
+        theta           = theta + thetaDifference;
+
+        if abs( delta ) < 0.0001
+            converged = true;
+            break;
+        end
+    end
+end
+
+fprintf('Precision of the mini-batch descent without regularization = %f\n', precision(X_Test, Y_Test, theta));
+
+figure();
+subplot( 2, 2, 1 );
+histogram( abs( theta( 1, 1:101 ) ), 20 );
+title( { 'Original Parameters Histogram (Y = 1)'; 'No Regularization' } );
+xlabel( 'Weight');
+ylabel( 'Occurrences');
+
+subplot( 2, 2, 2 );
+histogram( abs( theta(2, 1:101 ) ), 20 );
+title( { 'Original Parameters Histogram (Y = 2)'; 'No Regularization' } );
+xlabel( 'Weight');
+ylabel( 'Occurrences');
+
+subplot( 2, 2, 3 );
+histogram( abs( theta( 3, 1:101 ) ), 20 );
+title( {'Original Parameters Histogram (Y = 3)'; 'No Regularization' } );
+xlabel( 'Weight' );
+ylabel( 'Occurrences' );
+
+subplot( 2, 2, 4 );
+histogram( abs( theta( 4, 1:101 ) ), 20 );
+title( { 'Original Parameters Histogram (Y = 4)'; 'No Regularization' } );
+xlabel( 'Weight' );
+ylabel( 'Occurrences' );
+
+
+figure();
+histogram( abs( theta( :, 102:end ) ), 20 );
+title( { 'Random Parameters Histogram'; 'No Regularization' } );
+xlabel( 'Weight ');
+ylabel( 'Occurrences' );
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    Mini-Batch with Regularization    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%theta           = rand(4, 201) - 0.5;
+miniBatchSize   = 568;
+alpha           = 0.6;
+thetaDifference = zeros(4, 201);
+lambda1         = 0.016;
+lambda2         = 0.046;
+itr             = 0;
+logLikelihood   = -realmax;
+
+converged = false;
+while ~converged
+    itr = itr + 1;
+    [X_Batch, Y_Batch] = create_mini_batches(X_Learning, Y_Learning, miniBatchSize);
+
+    learning_Rate = 2 / itr;
+    for i = 1:size( X_Batch, 2)
+        tempLogLikelihood   = sum( ( sum( ( ( Y_Validation * theta ) .* X_Validation' )' ) ) - log( sum( exp( eye(4) * theta * X_Validation ) ) ) );
+        delta               = tempLogLikelihood - logLikelihood;
+        logLikelihood       = tempLogLikelihood;
+
+        expected        = ( ( exp( eye(4) * theta * X_Batch{:,i} ) ) ./ ( repmat( sum( exp( eye(4) * theta * X_Batch{:,i} ) ), 4, 1 ) ) ) * X_Batch{:,i}';
+        batch           = Y_Batch{i, :}' * X_Batch{:, i}';
+        gradient        = ( ( expected - batch ) ./ miniBatchSize ) + ( ( miniBatchSize / size( X_Learning, 2 ) ) * ((lambda1 * 2 * theta ) + ( lambda2 * ( ( ( theta > 0 ) + ( theta < 0 ) * -1 ) ) ) ) );
+        thetaDifference = ( alpha * thetaDifference ) - ( learning_Rate * gradient );
+        theta           = theta + thetaDifference;
+
+        if abs( delta ) < 0.01
+            converged = true;
+            break;
+        end
+    end
+end
+
+fprintf('Precision of the mini-batch descent with regularization = %f\n', precision(X_Test, Y_Test, theta));
+
+figure();
+subplot( 2, 2, 1 );
+histogram( abs( theta( 1, 1:101 ) ), 20 );
+title( { 'Original Parameters Histogram (Y = 1)'; 'With Regularization' } );
+xlabel( 'Weight');
+ylabel( 'Occurrences');
+
+subplot( 2, 2, 2 );
+histogram( abs( theta(2, 1:101 ) ), 20 );
+title( { 'Original Parameters Histogram (Y = 2)'; 'With Regularization' } );
+xlabel( 'Weight');
+ylabel( 'Occurrences');
+
+subplot( 2, 2, 3 );
+histogram( abs( theta( 3, 1:101 ) ), 20 );
+title( {'Original Parameters Histogram (Y = 3)'; 'With Regularization' } );
+xlabel( 'Weight' );
+ylabel( 'Occurrences' );
+
+subplot( 2, 2, 4 );
+histogram( abs( theta( 4, 1:101 ) ), 20 );
+title( { 'Original Parameters Histogram (Y = 4)'; 'With Regularization' } );
+xlabel( 'Weight' );
+ylabel( 'Occurrences' );
+
+
+figure();
+histogram( abs( theta( :, 102:end ) ), 20 );
+title( { 'Random Parameters Histogram'; 'With Regularization' } );
+xlabel( 'Weight ');
+ylabel( 'Occurrences' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
